110dbafbbf709de38236820ca8b2f7f3912cc580
Devika Kumar <devikaku@usc.edu>
2018-10-15 19:40:59 -0700

    goal/activity view work--need to fix display for goal view and add functionality

--- app/src/main/java/charstars/uscfit/GoalsDisplay.java
+++ app/src/main/java/charstars/uscfit/GoalsDisplay.java
@@ -1,10 +1,16 @@
 package charstars.uscfit;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -14,6 +20,8 @@ import android.widget.ProgressBar;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -25,13 +33,19 @@ public class GoalsDisplay extends AppCompatActivity implements View.OnClickListe
     private String email;
     private List<Goal> defaultGoals = new ArrayList<Goal>();
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //setting sample goal list (hardcoded)
         Goal sample1 = new Goal("get more fit", "workouts", new HashMap<Activity, Date>());
         Activity sample = new Activity("1 mile", 100, "Running");
+        Activity sample22 = new Activity("2 mile", 200, "Running");
         sample1.addActivity(sample, new Date(110));
+        sample1.addActivity(sample22, new Date(500));
         Goal sample2 = new Goal("take 100 steps", "steps", new HashMap<Activity, Date>());
         defaultGoals.add(sample1);
         defaultGoals.add(sample2);
@@ -50,7 +64,13 @@ public class GoalsDisplay extends AppCompatActivity implements View.OnClickListe
         }
 
         setContentView(R.layout.activity_goals_display);
+        createTable();
 
+
+
+    }
+
+    public void createTable(){
         TableLayout ll = (TableLayout)findViewById(R.id.goalLayout);
 
         if(ll == null){
@@ -65,10 +85,10 @@ public class GoalsDisplay extends AppCompatActivity implements View.OnClickListe
             row.setLayoutParams(lp);
 
 
-           // ImageButton addBtn = new ImageButton(this);
+            // ImageButton addBtn = new ImageButton(this);
             //addBtn.setImageResource(R.drawable.add);
-           // ImageButton minusBtn = new ImageButton(this);
-           // minusBtn.setImageResource(R.drawable.minus);
+            // ImageButton minusBtn = new ImageButton(this);
+            // minusBtn.setImageResource(R.drawable.minus);
 
             TextView desc = new TextView(this);
             TextView cat = new TextView(this);
@@ -81,37 +101,38 @@ public class GoalsDisplay extends AppCompatActivity implements View.OnClickListe
             row.addView(desc);
             row.addView(cat);
             Button b = new Button(this);
+            b.setText("View Details");
             b.setTag(current);
             b.setOnClickListener(this);
             row.addView(b);
 
             /**
-            LinearLayout linlay = new LinearLayout(this);
-            linlay.setOrientation(LinearLayout.VERTICAL);
-            for(Map.Entry<Activity, Date> entry: current.getDueDates().entrySet()){
-                LinearLayout activ = new LinearLayout((this));
-                activ.setOrientation(LinearLayout.HORIZONTAL);
-                TextView name = new TextView(this);
-                name.setText(entry.getKey().getName());
-                TextView cat2 = new TextView(this);
-                cat2.setText(entry.getKey().getCategory());
-                TextView due = new TextView(this);
-                due.setText(entry.getValue().toString());
-                activ.addView(name);
-                activ.addView(cat2);
-                activ.addView(due);
+             LinearLayout linlay = new LinearLayout(this);
+             linlay.setOrientation(LinearLayout.VERTICAL);
+             for(Map.Entry<Activity, Date> entry: current.getDueDates().entrySet()){
+             LinearLayout activ = new LinearLayout((this));
+             activ.setOrientation(LinearLayout.HORIZONTAL);
+             TextView name = new TextView(this);
+             name.setText(entry.getKey().getName());
+             TextView cat2 = new TextView(this);
+             cat2.setText(entry.getKey().getCategory());
+             TextView due = new TextView(this);
+             due.setText(entry.getValue().toString());
+             activ.addView(name);
+             activ.addView(cat2);
+             activ.addView(due);
 
-                CheckBox c = new CheckBox(this);
-                c.setChecked(entry.getKey().isCompleted());
+             CheckBox c = new CheckBox(this);
+             c.setChecked(entry.getKey().isCompleted());
 
 
-                linlay.addView(activ);
-            }
+             linlay.addView(activ);
+             }
 
-            //row.addView(checkBox);
-          // row.addView(minusBtn);
-          //  row.addView(addBtn);
-            row.addView(linlay);
+             //row.addView(checkBox);
+             // row.addView(minusBtn);
+             //  row.addView(addBtn);
+             row.addView(linlay);
 
              **/
 
@@ -121,14 +142,45 @@ public class GoalsDisplay extends AppCompatActivity implements View.OnClickListe
             ll.addView(row,i);
         }
 
-
-
     }
 
     @Override
     public void onClick(View v) {
+
+        if(v.getId() == R.id.addActivity){
+
+            return;
+        }
+        if(v.getId() == R.id.goback){
+            setContentView(R.layout.activity_goals_display);
+            createTable();
+            return;
+        }
+
         Goal g = (Goal) v.getTag();
         Log.d("goalsDisplay",g.getCategory());
 
+        setContentView(R.layout.goal_details);
+
+        TextView title = (TextView) findViewById(R.id.title);
+        title.setText("GOAL: " + g.getCategory() + "-- " + g.getDescription());
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(g.getDueDates());
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(mAdapter);
+
     }
+
+
 }
---
+++ app/src/main/java/charstars/uscfit/MyAdapter.java
@@ -0,0 +1,66 @@
+package charstars.uscfit;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Date;
+import java.util.Map;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private Map<Activity, Date> mDataset;
+    private Activity[] keyList;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView name, category, duedate;
+        public MyViewHolder(View v) {
+            super(v);
+            name = (TextView) v.findViewById(R.id.name);
+            category = (TextView) v.findViewById(R.id.category);
+            duedate = (TextView) v.findViewById(R.id.duedate);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(Map<Activity, Date> mDataset) {
+        this.mDataset = mDataset;
+        this.keyList = new Activity[mDataset.keySet().size()];
+        this.keyList = mDataset.keySet().toArray(keyList);
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_list_row, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        Activity activity = keyList[position];
+        holder.name.setText(activity.getName());
+        holder.category.setText(activity.getCategory());
+        holder.duedate.setText(mDataset.get(activity).toString());
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return keyList.length;
+    }
+}
