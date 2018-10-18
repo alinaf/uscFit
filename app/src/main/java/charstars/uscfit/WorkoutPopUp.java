7b39ef214eba0c9c7ba5aaefdfc8e0d86b4cd16c
aamarnath2 <aamarnat@usc.edu>
2018-10-17 23:55:56 -0700

    added popup windo for add workout

---
+++ app/src/main/java/charstars/uscfit/WorkoutPopUp.java
@@ -0,0 +1,13 @@
+package charstars.uscfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkoutPopUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_pop_up);
+    }
+}
