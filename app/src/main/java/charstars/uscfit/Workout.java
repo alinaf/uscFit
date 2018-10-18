e372e49ad5012d5e0f345914269e7fade2c39867
aamarnath2 <aamarnat@usc.edu>
2018-10-17 21:28:30 -0700

    updated WorkoutList based on new Workout Class. Also changed from using Calendar to Date

--- app/src/main/java/charstars/uscfit/Workout.java
+++ app/src/main/java/charstars/uscfit/Workout.java
@@ -1,5 +1,7 @@
 package charstars.uscfit;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -10,15 +12,16 @@ public class Workout {
     private Activity activity;
     private Quantifier quant;
     private int length;
-    Calendar calendar;
+    Date calendar;
+    private int caloriesBurned;
 
     public Workout(Activity action, Quantifier quant, int length, int yr, int mnth, int dte, int hr, int min) {
         this.activity = action;
         this.quant = quant;
         this.length = length;
         completed = false;
-        calendar.set(yr, mnth, dte, hr, min);
-
+        calendar = Calendar.getInstance().getTime();
+        caloriesBurned = action.getDefaultCalorieValue();
     }
 
     public boolean isCompleted() {
@@ -31,11 +34,18 @@ public class Workout {
     public void setActivity(Activity action) { this.activity = action; }
     public Activity getActivity() { return this.activity;}
 
+    public void setLength(int len) { this.length = len; }
+    public int getLength() { return this.length; }
+
     public void setQuant(Quantifier quant) { this.quant = quant; }
     public Quantifier getQuant () { return this.quant; }
 
-    public void setDate(int yr, int mnth, int dte, int hr, int min) { calendar.set(yr, mnth, dte, hr, min); }
-    public Calendar getDate() {
-        return calendar;
+    //public void setDate(int yr, int mnth, int dte, int hr, int min) { calendar.set(yr, mnth, dte, hr, min); }
+    public String getDate() {
+        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String today = formatter.format(calendar);
+        return calendar.toString();
     }
+
+    public int getCaloriesBurned() { return this.caloriesBurned; }
 }
