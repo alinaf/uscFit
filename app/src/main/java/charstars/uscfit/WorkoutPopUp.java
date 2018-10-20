10dc6e2c4d81fa1724927b2bfafa3864b32d3dc7
aamarnath2 <aamarnat@usc.edu>
2018-10-20 02:25:07 -0700

    only got touchups and database for workouts left

--- app/src/main/java/charstars/uscfit/WorkoutPopUp.java
+++ app/src/main/java/charstars/uscfit/WorkoutPopUp.java
@@ -1,20 +1,37 @@
 package charstars.uscfit;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import charstars.uscfit.DataHandlers.UpdateWorkouts;
 
-public class WorkoutPopUp extends AppCompatActivity {
+public class WorkoutPopUp extends AppCompatActivity implements View.OnClickListener {
     Workout w;
     private String email;
+
+
+    Button btnDatePicker, btnTimePicker;
+    EditText txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    private int eYear, eMonth, eDay, eHour, eMinute;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +40,10 @@ public class WorkoutPopUp extends AppCompatActivity {
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
+       // int width = dm.widthPixels;
+       // int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.8), (int)(height*.6));
+       // getWindow().setLayout((int)(width*.8), (int)(height*.6));
 
         Intent intent = getIntent();
         if (savedInstanceState == null) {
@@ -40,8 +57,16 @@ public class WorkoutPopUp extends AppCompatActivity {
             email = (String) savedInstanceState.getSerializable("EMAIL");
         }
 
-
         initializeFields();
+
+        btnDatePicker=(Button)findViewById(R.id.btn_date);
+        btnTimePicker=(Button)findViewById(R.id.btn_time);
+        txtDate=(EditText)findViewById(R.id.in_date);
+        txtTime=(EditText)findViewById(R.id.in_time);
+
+        btnDatePicker.setOnClickListener(this);
+        btnTimePicker.setOnClickListener(this);
+
     }
 
     public void initializeFields(){
@@ -58,8 +83,63 @@ public class WorkoutPopUp extends AppCompatActivity {
         Activity activity = new Activity(((Spinner)findViewById(R.id.workoutSpinner)).getSelectedItem().toString(), 50);
         int length = (((NumberPicker)findViewById(R.id.lengthPicker)).getValue());
         Quantifier quant = Quantifier.valueOf(((Spinner)findViewById(R.id.quantifierOption)).getSelectedItem().toString());
-        Workout workout = new Workout(activity, quant, length);
+        GregorianCalendar cal = new GregorianCalendar(eYear, eMonth, eDay, eHour, eMinute);
+        Workout workout = new Workout(activity, quant, length, cal);
         UpdateWorkouts.addWorkout(workout,email);
         finish();
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            eYear = year;
+                            eMonth = monthOfYear;
+                            eDay = dayOfMonth;
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        if (v == btnTimePicker) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            txtTime.setText(hourOfDay + ":" + minute);
+                            eHour = hourOfDay;
+                            eMinute = minute;
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
     }
 }
