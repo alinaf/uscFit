f45ffe73508491398c364dd31cce2ad8b856e419
qianze <qianzezh@usc.edu>
2018-10-29 19:22:23 -0700

    black box tests for steps

---
+++ app/src/test/java/charstars/uscfit/StepsTest.java
@@ -0,0 +1,27 @@
+package charstars.uscfit;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+
+import charstars.uscfit.DataHandlers.UpdateWorkouts;
+
+import static org.junit.Assert.*;
+
+public class StepsTest {
+
+    StepsDisplay sd;
+
+    @Before
+    public void setUp() throws Exception {
+        sd = new StepsDisplay();
+    }
+
+    @Test
+    public void addActivityNotInSpinner() {
+
+    }
+
+}
